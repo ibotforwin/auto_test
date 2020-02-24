956ffabcba83c5e2e9f64a36d3bcdf67efa87926
Kay <ibotforwin@gmail.com>
2020-02-24 13:50:52 -0500

    This is a working early beta of an automated function tester for Python.
    The example is in tester.py, while the actual code lies in auto_test.py!
    
    Signed-off-by: Kay <ibotforwin@gmail.com>

--- auto_test.py
+++ auto_test.py
@@ -11,34 +11,37 @@ supported_types = ['int', 'float', 'str']
 
 autotest_result = {}
 
-
+#A sample test that returns the anticipated type
 def int_ret(number: int) -> str:
     string = "cactusmonster"
     return string
 
+#A sample test that returns the wrong type
 def false_test(number: int) -> str:
     floating = 3.2222
     return floating
 
-def test_typematching():
-    for name in list(globals()):
+
+
+def test_typematching(glob):
+    for name in list(glob):
         if not name.startswith('__'):
             try:
-                return_type = str((globals()[name].__annotations__)['return'])
+                return_type = str((glob[name].__annotations__)['return'])
                 autotest_result.update({name: return_type.replace("<class '", "").replace("'>", "")})
             except:
                 continue
 
     for func in autotest_result:
         if autotest_result[func] != None:
-            this_func = globals()[func].__annotations__
+            this_func = glob[func].__annotations__
             for arg in this_func:
                 if arg != 'return':
                     input_type = str(this_func[arg]).replace("<class '", "").replace("'>", "")
                     for available in supported_types:
                         if available == input_type:
-                            func_return = globals()[func]("test_" + input_type)
-                            func_return = globals()[func]("test_" + input_type)
+                            func_return = glob[func]("test_" + input_type)
+                            func_return = glob[func]("test_" + input_type)
                             actual_return_type = str(type(func_return)).replace("<class '", "").replace("'>", "")
                             if actual_return_type == autotest_result[func]:
                                 autotest_result[func] = 'Passed'
@@ -47,4 +50,4 @@ def test_typematching():
     return autotest_result
 
 
-test_typematching()
+
--- tester.py
+++ tester.py
@@ -1,8 +1,10 @@
 from auto_test import test_typematching
 
-print(test_typematching())
-def int_ret(number: int) -> str:
+
+def int_ret_newfile(number: int) -> str:
     string="cactusmonster"
     # print(string)
     # return type(number)
     return string
+
+print(test_typematching(globals()))
